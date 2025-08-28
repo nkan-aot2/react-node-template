4249eca9679ade0b5758ce2ccff4acf3d1ff8126
unknown <nicholas.kan@aot-technologies.com>
2025-08-28 14:04:59 -0700

    test env url

--- web/src/services/userService.tsx
+++ web/src/services/userService.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/user/';
+const API_URL = import.meta.env.VITE_API_BASE_URL + '/user/';
+
 
 interface User {
     firstName: string;
